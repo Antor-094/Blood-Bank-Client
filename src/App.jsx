e037578531674e1152044717457f38c86e4649f5
Antor-094 <mdantor556677889@gmail.com>
2023-08-03 15:29:16 +0600

    antd and taiwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from 'antd';
+
+
+function App() {
+ 
+
+  return (
+    <div>
+         <h1>BloodConnect+</h1>
+         <Button type="primary">Primary Button</Button>
+
+    </div>
+  )
+}
+
+export default App
+ 
