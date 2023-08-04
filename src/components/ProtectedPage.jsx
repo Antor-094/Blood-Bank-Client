c6fdc78cf770e12e0121ee91cae93d923300a3ac
Antor-094 <mdantor556677889@gmail.com>
2023-08-04 11:41:46 +0600

    Donar navbar added

--- src/components/ProtectedPage.jsx
+++ src/components/ProtectedPage.jsx
@@ -42,9 +42,39 @@ const ProtectedPage = ({ children }) => {
   }, []);
   return (
     currentUser && (
-      <div>
-        {currentUser && <h1> Welcome {getLoggedInUserName(currentUser)}</h1>}
-        {children}
+        <div>
+        {/* header */}
+        <div className="flex justify-between items-center bg-primary text-white px-5 py-3 mx-5 rounded-b">
+          <div onClick={() => navigate("/")} className="cursor-pointer">
+            <h1 className="text-2xl">BloodConnect+</h1>
+            <span className="text-xs">
+              {currentUser.userType.toUpperCase()}
+            </span>
+          </div>
+
+          <div className="flex items-center gap-1">
+          <i className="ri-shield-user-line"></i>
+            <div className="flex flex-col">
+              <span
+                className="mr-5 text-md  cursor-pointer"
+                onClick={() => navigate("/profile")}
+              >
+                {getLoggedInUserName(currentUser).toUpperCase()}
+              </span>
+            </div>
+
+            <i
+              className="ri-logout-circle-r-line ml-5 cursor-pointer"
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/login");
+              }}
+            ></i>
+          </div>
+        </div>
+
+        {/* body */}
+        <div className="px-5 py-5">{children}</div>
       </div>
     )
   );
